73c25d5fe3ce5004202c080276e318a7da70605d
Jesus M <jmolinap2@unemi.edu.ec>
2024-08-18 09:12:47 -0500

    ward

--- drf/base.py
+++ drf/base.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import dj_database_url
 from environ import Env
+
 env = Env()
 Env.read_env()
 ENVIRONMENT = env('ENVIRONMENT', default='production')
@@ -14,13 +15,6 @@ else:
     DEBUG = False
     ALLOWED_HOSTS = ['*']
 
-
-
-# Configuraciones de la base de datos (se sobreescribirán en producción)
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 # Configuración de la base de datos
 if ENVIRONMENT == 'development':
     DATABASES = {
@@ -32,9 +26,16 @@ if ENVIRONMENT == 'development':
             'PORT': env('DATABASE_PORT'),
         }
     }
+    ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
+    print('Entorno:', ENVIRONMENT)
+    print('Configuración de la base de datos:', DATABASES['default'])
+    print('ALLOWED_HOSTS:', ALLOWED_HOSTS)
 
 if ENVIRONMENT == 'production':
-    DATABASES['default'] = dj_database_url.parse('postgresql://postgres:dMGvaakLJELZvTAacbQXowPiEZsWEZit@monorail.proxy.rlwy.net:15900/railway')
-print('Entorno: ',ENVIRONMENT )
+    DATABASES['default'] = dj_database_url.parse(env('DATABASE_URL'))
+
+
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
